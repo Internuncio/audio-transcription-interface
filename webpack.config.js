f9b51c5581a1aa38ac25479a01cffc44c1d4251a
Nolan Tait <nolanjtait@gmail.com>
2018-09-26 15:13:11 -0700

    mend

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     contentBase: path.resolve(__dirname, 'public'),
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.js$/, exclude: /node_modules/, use: [{ loader: 'babel-loader' }] },
       { test: /(\.css)$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
     ],
